7c63f0e1e3f377341e3a4999e34bf5bec5df8d28
haikaljs <haikaljavascript@gmail.com>
2024-01-14 16:12:31 +0800

    Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div>Hello world</div>;
-}
+import React from "react";
+import Header from "@/components/Header";
+
+const Home = () => {
+  return (
+    <>
+      <Header label="Home" />
+    </>
+  );
+};
+
+export default Home;
