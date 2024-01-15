311b7317070195e2d8fc5989bd7543265a315f3f
haikaljs <haikaljavascript@gmail.com>
2024-01-15 18:00:23 +0800

    register endpoint

---
+++ pages/api/register.tsx
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcrypt";
+import prismadb from "@/libs/prismadb";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+  try {
+    const { email, name, username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prismadb.user.create({
+      data: {
+        email,
+        name,
+        username,
+        hashedPassword,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
