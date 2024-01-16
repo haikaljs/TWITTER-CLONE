1532dfd799f3a244345f4e9d56e1ecc670c39738
haikaljs <haikaljavascript@gmail.com>
2024-01-17 00:20:23 +0800

    Create posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "@/components/Header";
+import Form from "@/components/Form";
 
 const Home = () => {
   return (
     <>
       <Header label="Home" />
+      <Form placeholder="What's happening" />
     </>
   );
 };
